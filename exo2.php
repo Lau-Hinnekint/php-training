c13f0fa89402739183a543c8f4f76894029f7010
Guillaume Belleuvre <guillaumebelleuvre@gmail.com>
2023-06-19 14:36:46 +0200

    exo2 questions 6 to 10

--- exo2.php
+++ exo2.php
@@ -125,7 +125,15 @@ $fruits = ["fraise", "banane", "pomme", "cerise", "abricot", "pêche", "ananas",
             <h2 class="exercice-ttl">Question 6</h2>
             <p class="exercice-txt">Afficher les fruits dans un ordre aléatoire</p>
             <div class="exercice-sandbox">
+                <ul>
+                    <?php
+                    shuffle($fruits);
+                    for ($i = 0; $i < sizeof($fruits); $i++) {
+                        echo "<li>$fruits[$i]</li>";
+                    }
 
+                    ?>
+                </ul>
             </div>
         </section>
 
@@ -134,7 +142,14 @@ $fruits = ["fraise", "banane", "pomme", "cerise", "abricot", "pêche", "ananas",
             <h2 class="exercice-ttl">Question 7</h2>
             <p class="exercice-txt">Afficher les fruits dont la chaîne de caractère est composée de 5 caractères au maximum</p>
             <div class="exercice-sandbox">
-
+                <ul>
+                    <?php
+                    foreach ($fruits as $fruit) {
+                        if (strlen($fruit) <= 5) {
+                            echo "<li>$fruit</li>";
+                        }
+                    }
+                    ?></ul>
             </div>
         </section>
 
@@ -147,7 +162,11 @@ $fruits = ["fraise", "banane", "pomme", "cerise", "abricot", "pêche", "ananas",
             <p class="exercice-txt">Dans la phrase suivante : "<?= $breakfast ?>"</p>
             <p class="exercice-txt">Remplacez pomme par pêche et banane par mangue et affichez-la.</p>
             <div class="exercice-sandbox">
-
+                <?php
+                $oldwords = ['pomme', 'banane'];
+                $newwords = ['pêche', 'mangue'];
+                echo str_replace($oldwords, $newwords, $breakfast);
+                ?>
             </div>
         </section>
 
@@ -156,7 +175,17 @@ $fruits = ["fraise", "banane", "pomme", "cerise", "abricot", "pêche", "ananas",
             <h2 class="exercice-ttl">Question 9</h2>
             <p class="exercice-txt">Affichez la chaîne de caractère composée de l'ensemble des fruits de la liste, séparés par une virgule et un espace.</p>
             <div class="exercice-sandbox">
+                <?php
+                foreach ($fruits as $index => $fruit) {
+                    echo $fruit;
+                    if ($index < sizeof($fruits) - 1) {
+                        echo ", ";
+                    }
+                }
+                echo "<br>";
 
+                echo implode(", ", $fruits);
+                ?>
             </div>
         </section>
 
@@ -170,7 +199,24 @@ $fruits = ["fraise", "banane", "pomme", "cerise", "abricot", "pêche", "ananas",
             <h2 class="exercice-ttl">Question 10</h2>
             <p class="exercice-txt">Afficher dans une liste HTML tous les fruits de la liste qui apparaissent dans la phrase suivante : "<?= $salad ?>"</p>
             <div class="exercice-sandbox">
-
+                <ul>
+                    <?php
+                    foreach ($fruits as $fruit) {
+                        if (str_contains($salad, $fruit)) {
+                            echo "<li> $fruit </li> ";
+                        }
+                    }
+                    ?>
+                </ul>
+                <ul>
+                    <?php
+                    foreach ($fruits as $fruit) {
+                        if (strpos($salad, $fruit) !== false) {
+                            echo "<li> $fruit </li> ";
+                        }
+                    }
+                    ?>
+                </ul>
             </div>
         </section>
     </div>
